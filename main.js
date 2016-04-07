ab8ac0300267a5b663a268b4755d66ebc2b00197
juneberry <jshin06@risd.edu>
2016-04-06 22:42:22 -0400

    shift

---
+++ main.js
@@ -0,0 +1,179 @@
+
+$(".A").hover(function() {
+  $(this).attr("src", "AE300.gif" );
+	},function() {
+  	$(this).attr("src", "A2.jpg");
+  });
+
+
+$(".B").hover(function() {
+  $(this).attr("src", "BD.gif" );
+	},function() {
+  	$(this).attr("src", "B.jpg");
+  });
+
+$(".C").hover(function() {
+  $(this).attr("src", "CV.gif" );
+	},function() {
+  	$(this).attr("src", "C.jpg");
+  });
+
+$(".D").hover(function() {
+  $(this).attr("src", "DB.gif" );
+	},function() {
+  	$(this).attr("src", "D.jpg");
+  });
+
+$(".E").hover(function() {
+  $(this).attr("src", "EA.gif" );
+	},function() {
+  	$(this).attr("src", "E.jpg");
+  });
+
+$(".F").hover(function() {
+  $(this).attr("src", "FN.gif" );
+	},function() {
+  	$(this).attr("src", "F.jpg");
+  });
+
+$(".G").hover(function() {
+  $(this).attr("src", "GT.gif" );
+	},function() {
+  	$(this).attr("src", "G.jpg");
+  });
+
+$(".H").hover(function() {
+  $(this).attr("src", "HW.gif" );
+	},function() {
+  	$(this).attr("src", "H.jpg");
+  });
+
+$(".I").hover(function() {
+  $(this).attr("src", "IO.gif" );
+	},function() {
+  	$(this).attr("src", "I.jpg");
+  });
+
+$(".J").hover(function() {
+  $(this).attr("src", "JQ.gif" );
+	},function() {
+  	$(this).attr("src", "J.jpg");
+  });
+
+$(".K").hover(function() {
+  $(this).attr("src", "KP.gif" );
+	},function() {
+  	$(this).attr("src", "K.jpg");
+  });
+
+$(".L").hover(function() {
+  $(this).attr("src", "LM.gif" );
+	},function() {
+  	$(this).attr("src", "L.jpg");
+  });
+
+$(".M").hover(function() {
+  $(this).attr("src", "ML.gif" );
+	},function() {
+  	$(this).attr("src", "M.jpg");
+  });
+
+$(".N").hover(function() {
+  $(this).attr("src", "NF.gif" );
+	},function() {
+  	$(this).attr("src", "N.jpg");
+  });
+
+$(".O").hover(function() {
+  $(this).attr("src", "OI.gif" );
+	},function() {
+  	$(this).attr("src", "O.jpg");
+  });
+
+$(".P").hover(function() {
+  $(this).attr("src", "PK.gif" );
+	},function() {
+  	$(this).attr("src", "P.jpg");
+  });
+
+$(".Q").hover(function() {
+  $(this).attr("src", "QJ.gif" );
+	},function() {
+  	$(this).attr("src", "Q.jpg");
+  });
+
+$(".R").hover(function() {
+  $(this).attr("src", "RS.gif" );
+	},function() {
+  	$(this).attr("src", "R.jpg");
+  });
+
+$(".S").hover(function() {
+  $(this).attr("src", "SR.gif" );
+	},function() {
+  	$(this).attr("src", "S.jpg");
+  });
+
+$(".T").hover(function() {
+  $(this).attr("src", "TG.gif" );
+	},function() {
+  	$(this).attr("src", "T.jpg");
+  });
+
+$(".U").hover(function() {
+  $(this).attr("src", "UY.gif" );
+	},function() {
+  	$(this).attr("src", "U.jpg");
+  });
+
+$(".V").hover(function() {
+  $(this).attr("src", "VC.gif" );
+	},function() {
+  	$(this).attr("src", "V.jpg");
+  });
+
+$(".W").hover(function() {
+  $(this).attr("src", "WH.gif" );
+	},function() {
+  	$(this).attr("src", "W.jpg");
+  });
+
+$(".X").hover(function() {
+  $(this).attr("src", "XZ.gif" );
+	},function() {
+  	$(this).attr("src", "X.jpg");
+  });
+
+$(".Z").hover(function() {
+  $(this).attr("src", "ZX.gif" );
+	},function() {
+  	$(this).attr("src", "Z.jpg");
+  });
+
+$(".period").hover(function() {
+  $(this).attr("src", "period-comma150.gif" );
+	},function() {
+  	$(this).attr("src", "period150.jpg");
+  });
+
+$(".comma").hover(function() {
+  $(this).attr("src", "comma-period150.gif" );
+	},function() {
+  	$(this).attr("src", "comma150.jpg");
+  });
+
+$("#title").hover(function(){
+   $(this).attr("src", "SHIFT3.png" );
+	},function() {
+  	$(this).attr("src", "SHIFT4.png");
+  });
+
+// $(".home-gif").ready(function(){
+//    $(".home-gif").css("width", 200 );
+// 	});
+
+$(".cem").hover(function(){
+   $(this).attr("src", "heycemreveal.png" );
+	},function() {
+  	$(this).attr("src", "heycemconceal.png");
+  });
